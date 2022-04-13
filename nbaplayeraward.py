062cead72c34c44cabd6849211e4c1438b294ff1
Orlando Aguilar <oaguilar@terpmail.umd.edu>
2022-04-13 17:05:42 -0400

    Fixed the docstring for the class.

--- nbaplayeraward.py
+++ nbaplayeraward.py
@@ -1,5 +1,6 @@
 class database:
-    """Database for our project discus
+    """Database for our project discussing NBA players
+    that are in the 75th Player Anniversary Team.
     """
     def __init__(self):
         pass
