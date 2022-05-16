e16bec545a376b6b7b82ce6f8b14ddeb6af349ed
hafizbarrie <102669768+hafizbarrie@users.noreply.github.com>
2022-05-16 18:51:49 -0400

    Add files via upload

---
+++ team_Y_favorite_players.py
@@ -0,0 +1,49 @@
+seasons_play = 19
+point_average = 27.0
+player_description = f'labron_james has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 15
+point_average = 30.1
+player_description = f'michael jordan has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 21
+point_average = 24.6
+player_description = f'Kareem abdul jabbar has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 13
+point_average = 19.5
+player_description = f'magic johnson has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 13
+point_average = 20.2
+player_description = f'larry bird has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 13
+point_average = 26.0
+player_description = f'stephen curry has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 14
+point_average = 27.1
+player_description = f'kevin durant has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 20
+point_average = 25.0
+player_description = f'kobe bryant has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 19
+point_average = 19.0
+player_description = f'Tim Duncan has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
+
+seasons_play = 17
+point_average = 16.1
+player_description = f'Scottie Pippen has played for  {seasons_play.upper()} and has average  {year_retire.upper()} per game'
+print(player_description)
